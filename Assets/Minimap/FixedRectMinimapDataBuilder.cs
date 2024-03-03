f3cde45d8e30e8a6516fc56b80dc36e95be95c0b
Qma <mk348437@gmail.com>
2024-03-03 17:30:13 +0100

    Implement minimap object add/remove

--- Assets/Minimap/FixedRectMinimapDataBuilder.cs
+++ Assets/Minimap/FixedRectMinimapDataBuilder.cs
@@ -9,5 +9,41 @@ namespace Minimap
 
         public IEnumerable<IMinimapObject> BuildObjects() =>
             _objects;
+
+        public void Add(MinimapObject minimapObject)
+        {
+            if (Contains(minimapObject))
+                return;
+            
+            _objects.Add(minimapObject);
+        }
+
+        public void Remove(MinimapObject minimapObject)
+        {
+            if (!Contains(minimapObject, out int index))
+                return;
+            
+            _objects.RemoveAt(index);
+        }
+
+        private bool Contains(MinimapObject minimapObject) =>
+            _objects.Contains(minimapObject);
+
+        private bool Contains(MinimapObject minimapObject, out int index)
+        {
+            for (var i = 0; i < _objects.Count; i++)
+            {
+                MinimapObject listObj = _objects[i];
+
+                if (!ReferenceEquals(listObj, minimapObject))
+                    continue;
+
+                index = i;
+                return true;
+            }
+
+            index = default;
+            return false;
+        }
     }
 }
