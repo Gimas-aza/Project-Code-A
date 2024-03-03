78f188d40e49c528a2a962d6256faeac0c0c5d87
Qma <mk348437@gmail.com>
2024-03-03 13:32:59 +0100

    Create minimap gizmos

--- Assets/Minimap/MinimapCenter.cs
+++ Assets/Minimap/MinimapCenter.cs
@@ -1,16 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Minimap
 {
     public class MinimapCenter : MonoBehaviour
     {
-        [SerializeField] private float _extent;
+        [SerializeField, Min(0)] private float _extent;
+        [SerializeField] private Color _gizmoColor;
         
         public float Extent => _extent;
 
 #if UNITY_EDITOR
-        private void OnDrawGizmosSelected() =>
-            Gizmos.DrawWireCube(transform.position, new Vector3(Extent * 2, 0, Extent * 2));
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = _gizmoColor;
+            
+            Vector3 pos = transform.position;
+            
+            var ext1 = new Vector3(_extent, 0, _extent);
+            var ext2 = new Vector3(-_extent, 0, _extent);
+            
+            Vector3 p1 = pos - ext1;
+            Vector3 p2 = pos + ext2;
+            Vector3 p3 = pos + ext1;
+            Vector3 p4 = pos - ext2;
+
+            ReadOnlySpan<Vector3> points = stackalloc Vector3[] { p1, p2, p2, p3, p3, p4, p4, p1 };
+            
+            Gizmos.DrawLineList(points);
+        }
 #endif
     }
 }
